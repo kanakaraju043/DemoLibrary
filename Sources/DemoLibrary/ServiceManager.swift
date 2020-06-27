ae9f36c21dde55af4d05b01aeb01ab3948cba461
Kanakaraju Gandreddi <Kanakaraju.Gandreddi@ril.com>
2020-06-27 18:06:49 +0530

    initial commit

---
+++ Sources/DemoLibrary/ServiceManager.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Kanakaraju Gandreddi on 27/06/20.
+//
+
+import Foundation
+public struct ServiceManager {
+    
+    public static func getSDKName() ->String? {
+        return "DemoLibrary"
+    }
+}
